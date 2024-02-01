c504579e467dd783833c25f625f82a296bd18ed6
tarkeshgoswami3 <154048438+tarkeshgoswami3@users.noreply.github.com>
2024-02-02 00:37:44 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,19 +1,19 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 1234567890))
+    API_ID = int(env.get("29689475", 1234))
+    API_HASH = env.get("e550c215a6720df4c747ddf8febf1686", "xyz")
+    OWNER_ID = int(env.get("5882105086", 1234567890))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
+    BOT_USERNAME = env.get("animelovers3bot", "BotFather")
+    BOT_TOKEN = env.get("6510583050:AAGkF0QUAf9jQwq5Cs5gN1xs3HfFNQlVFfo", "1234:abcd")
+    CHANNEL_ID = int(env.get("-1001316599974", -1001234567890))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "https://animelovers3-1f93b4af25a6.herokuapp.com/")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8080))
+    PORT = int(env.get("PORT", 8080)
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
